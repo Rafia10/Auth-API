f040eeb7a2baf259771e553fd5e88e6e15ba38a9
Rafia10 <parasramsha@gmail.com>
2022-02-23 23:36:09 -0500

    update changes

--- api/controller/validation.js
+++
@@ -1,5 +0,0 @@
-const { body } = require("express-validator");
-function validation() {
-  body("email").isEmail(), body("password").isLength({ min: 9 });
-}
-module.exports = { validation };
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,11 +1,16 @@
 const express = require("express");
 
-const { validation } = require("../controller/validation");
+const { body } = require("express-validator");
 const { userSignup } = require("../controller/userSignup");
 const { userLogin } = require("../controller/userLogin");
 const router = express.Router();
 
-router.post("/signup", validation, userSignup);
+router.post(
+  "/signup",
+  body("email").isEmail(),
+  body("password").isLength({ min: 9 }),
+  userSignup
+);
 
 router.post("/login", userLogin);
 
